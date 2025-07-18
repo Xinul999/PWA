af84e7e2b4c61b04a15a10b35a655e63efbde496
Xinul999 <guichardjulien@hotmail.fr>
2025-07-18 16:58:32 +0200

    /sw.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 /*On verifie que le service worker est disponible dans le navigateur*/
 if(navigator.serviceWorker){
-    navigator.serviceWorker.register('sw.js').then((registration) =>{
+    navigator.serviceWorker.register('/sw.js').then((registration) =>{
         console.log('Service Worker enregistrer: ', registration.scope);
     }).catch(registrationError => {
         console.log('Erreur enregistrement du Service Worker:', registrationError);
